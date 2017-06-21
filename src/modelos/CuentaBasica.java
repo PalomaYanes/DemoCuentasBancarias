38465537ad6d5f0c3de3de0d0d843abbd0f9b623
Paloma del Rosario Yanes Cach <16470313@itcampeche.edu.mx>
2017-06-20 19:11:42 -0500

    Proyecto con solo la implementacion de la clase CuentaDeAhorro.

--- src/modelos/CuentaBasica.java
+++ src/modelos/CuentaBasica.java
@@ -5,10 +5,51 @@
  */
 package modelos;
 
+
+
 /**
  *
- * @author palom
+ * @author paloma
  */
 public class CuentaBasica {
+   
+    
+    int numeroDeCuenta; 
+    private String cliente=null;
+    public double saldo;
+    public double cantidad;
+    public double retiro;
+
+    
+    public void CuentaBasica (int numeroDeCuenta , String cliente, int saldo)
+    {
+        this.numeroDeCuenta = numeroDeCuenta;
+        this.cliente = cliente;
+        this.saldo =    saldo;
+      
+    }
+    
+
+    public double getSaldo()
+    {
+        return this.saldo;
+    }
+    
+    public double depositar(double deposito)
+    {
+        this.cantidad=deposito;
+        saldo= saldo + cantidad;
+        return saldo;
+    }
+    
+    public double retirar (double retirar)
+    {
+        
+        this.retiro=retirar;
+        saldo=saldo-retiro;
+            
+         return saldo;
+    }
     
 }
+
